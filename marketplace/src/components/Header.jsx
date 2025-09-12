42e21ebc779623d04624c2a5981682850803d5e3
Akechi Goro <zane41181@gmail.com>
2025-09-12 10:46:55 +0300

    cdwopcmdcwmcdwklm

---
+++ marketplace/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className="topbar">
+      <div className="logo">
+        <div className="mark">R/M</div>
+        <div>
+          <div className='main-title'>RUDN Market</div>
+          <div  className="alter-title">
+            Классические объявления
+          </div>
+        </div>
+      </div>
+
+      <nav className="nav-btns">
+        <button className="btn">Избранное</button>
+        <button className="btn">Сообщения</button>
+        <button className="btn">Войти</button>
+      </nav>
+    </header>
+  );
+}
