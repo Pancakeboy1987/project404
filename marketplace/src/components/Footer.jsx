8682297f44de171a2064d89daacf99359e559f15
Akechi Goro <zane41181@gmail.com>
2025-09-16 21:45:39 +0300

    //

---
+++ marketplace/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <div
+          className="footer-block">
+          <div className="footer-block-text">
+            <a href="#">О нас</a>
+            <a href="#">Помощь</a>
+            <a href="#">Контакты</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
