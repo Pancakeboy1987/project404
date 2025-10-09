b1e30d27cece2fd975019b856a7ff19fd7f9d82b
Akechi Goro <zane41181@gmail.com>
2025-10-09 17:39:49 +0300

    ь

--- server/models/models.js
+++ server/models/models.js
@@ -1,22 +1,20 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
 
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }, //здесь юник айди будет применятся для всех новых созданных объектов
+  email: { type: DataTypes.STRING, primaryKey: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
 
-const User = sequelize.define('user',{
-    id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},//здесь юник айди будет применятся для всех новых созданных объектов
-    email:{type: DataTypes.STRING, primaryKey:true, },
-    password:{type: DataTypes.STRING},
-    role:{type:DataTypes.STRING,defaultValue:"USER"}
-})
-
-const Product = sequelize.define('Product',{
-    id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    title:{type: DataTypes.STRING, primaryKey:true, },
-    price:{type: DataTypes.INTEGER},
-    location:{type:DataTypes.STRING},
-    description:{type:DataTypes.STRING},
-    image:{type:DataTypes.STRING},
-
-})
+const Product = sequelize.define("Product", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, primaryKey: true },
+  price: { type: DataTypes.INTEGER },
+  location: { type: DataTypes.STRING },
+  description: { type: DataTypes.STRING },
+  image: { type: DataTypes.STRING },
+});
 
 //ниже опишу связи но для начала у нас только два объекта
